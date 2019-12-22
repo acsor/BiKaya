c66a167d412a2c30555620c9a856a6dfa22e10f9
Acsor <nildexo@yandex.com>
2019-12-22 12:42:37 +0100

    Add IO primitives for reading from terminal and printing out to printer devices
    Some internal code in src/io.c has undergone modifications.

--- include/io.h
+++ include/io.h
@@ -1,14 +1,41 @@
-#ifndef __TERMPRINT_H__
-#define __TERMPRINT_H__
+#ifndef BKA_IO
+#define BKA_IO
 
 
 /**
  * @param term Terminal register to write to
- * @param str  Series of characters to send into @c term
- * @return @c E_GENERIC if errors occurred, count of written characters
+ * @param str  Null-terminated string to write into @c term
+ * @return @c BKA_E_GEN if errors occurred, count of written characters
  * otherwise.
  */
 int bka_term_puts(termreg_t *term, char const *str);
+/**
+ * Reads a single character from @c term.
+ * @return @c BKA_E_GEN if errors occurred, number of read characters
+ * otherwise.
+ */
+int bka_term_recvc(termreg_t *term);
+/**
+ * Reads at most <code>length - 1</code> characters from @c term into @c
+ * dest, stopping earlier if a newline character is met. A @c '\0'
+ * terminating character is stored at the end of the string (which is why
+ * only <code>length - 1</code> effective characters are stored).
+ * @param term Terminal device to read from
+ * @param dest Destination string to write to
+ * @param length Maximum number of characters to read, @c '\0' included
+ * @return @c BKA_E_GEN in case of errors, @c BKA_E_OK otherwise.
+ */
+int bka_term_recvs(termreg_t *term, char *dest, unsigned length);
+
+
+/**
+ *
+ * @param dev
+ * @param str Null-terminated string to write into @c dev
+ * @return @c BKA_E_GEN if errors occurred, count of written characters
+ * otherwise.
+ */
+int bka_print_puts(dtpreg_t *dev, char const *str);
 
 
 #endif
--- src/io.c
+++ src/io.c
@@ -1,51 +1,66 @@
 #include "sys.h"
 
-#define ST_READY           1
-#define ST_BUSY            3
-#define ST_TRANSMITTED     5
+#define TERM_ST_NOTINST		0
+#define TERM_ST_READY       1
+#define TERM_ST_OPC			2
+#define TERM_ST_BUSY        3
+#define TERM_ST_TRANSME		4
+#define TERM_ST_RECVE		4
+#define TERM_ST_TRANSMITTED	5
+#define TERM_ST_RECEIVED	5
 
-#define CMD_ACK            1
-#define CMD_TRANSMIT       2
-
-#define CHAR_OFFSET        8
-#define TERM_STATUS_MASK   0xFF
+#define TERM_CMD_RESET		0
+#define TERM_CMD_ACK		1
+#define TERM_CMD_TRANSM		2
+#define TERM_CMD_RECV		2
 
 /**
- * TODO Document.
- * @param tp
- * @return
+ * Generates a terminal device transmission command with argument @c c.
  */
-static unsigned int bka_term_status(termreg_t *tp) {
-    return ((tp->transm_status) & TERM_STATUS_MASK);
-}
+#define TERM_TRANSM_COMMAND(c)		((c << 8) | TERM_CMD_TRANSM)
+/**
+ * @return Terminal device transmit status of @c term.
+ */
+#define TERM_TRANSM_STATUS(term)	(term->transm_status & 0xFF)
+
+/**
+ * @return Terminal device receive status of @c term.
+ */
+#define TERM_RECV_STATUS(term)	(term->recv_status & 0xFF)
+/**
+ * @return The received character from a terminal device receive register.
+ */
+#define TERM_RECV_CHAR(term)	((term->recv_status & 0xFF00) >> 8)
+
+
+#define PRINT_ST_NOTINST	0
+#define PRINT_ST_READY		1
+#define PRINT_ST_OPC		2
+#define PRINT_ST_BUSY		3
+#define PRINT_ST_PRINTE		4
+
+#define PRINT_CMD_RESET		0
+#define PRINT_CMD_ACK       1
+#define PRINT_CMD_PRINTC    2
+
+#define PRINT_STATUS_MASK	0xFF
+
 
 /**
  * Issues a write of character @c c to terminal @c term.
  * @return @c BKA_E_GEN in case of errors, @c BKA_E_OK otherwise.
  */
-static int bka_term_putchar(termreg_t *term, char c) {
-    unsigned int stat;
+static int bka_term_putchar(termreg_t *term, char c);
 
-    stat = bka_term_status(term);
-    if (stat != ST_READY && stat != ST_TRANSMITTED)
-        return BKA_E_GEN;
-
-    term->transm_command = ((c << CHAR_OFFSET) | CMD_TRANSMIT);
-
-    while ((stat = bka_term_status(term)) == ST_BUSY)
-        ;
-
-    term->transm_command = CMD_ACK;
-
-    if (stat != ST_TRANSMITTED)
-        return BKA_E_GEN;
-    else
-        return BKA_E_OK;
-}
+/**
+ * Issues a write of character @c to printer device @c p.
+ * @return @c BKA_E_GEN in case of errors, @c BKA_E_OK otherwise.
+ */
+static int bka_print_putchar(dtpreg_t *p, char c);
 
 
 int bka_term_puts(termreg_t *term, char const *str) {
-	unsigned written;
+	int written;
 
 	for (written = 0; *str; written++, str++) {
 		if (bka_term_putchar(term, *str) != BKA_E_OK)
@@ -53,4 +68,86 @@ int bka_term_puts(termreg_t *term, char const *str) {
 	}
 
 	return written;
+}
+
+int bka_term_recvc(termreg_t *term) {
+	unsigned status = TERM_RECV_STATUS(term);
+	char read;
+
+	if (status != TERM_ST_READY && status != TERM_ST_RECEIVED)
+		return BKA_E_GEN;
+
+	term->recv_command = TERM_CMD_RECV;
+
+	while ((status = TERM_RECV_STATUS(term)) == TERM_ST_BUSY)
+		;
+
+	read = TERM_RECV_CHAR(term);
+	term->recv_command = TERM_CMD_ACK;
+
+	return status == TERM_ST_RECEIVED ? read: BKA_E_GEN;
+}
+
+int bka_term_recvs(termreg_t *term, char *dest, unsigned length) {
+	int in = -1;
+
+	while (length > 1 && in != '\n') {
+		if ((in = bka_term_recvc(term)) == BKA_E_GEN)
+			return BKA_E_GEN;
+
+		*dest = in;
+		dest++;
+		length--;
+	}
+
+	*dest = '\0';
+
+	return BKA_E_OK;
+}
+
+
+int bka_print_puts(dtpreg_t *dev, char const *str) {
+	int written;
+
+	for (written = 0; *str; written++, str++) {
+		if (bka_print_putchar(dev, *str) != BKA_E_OK)
+			return BKA_E_GEN;
+	}
+
+	return written;
+}
+
+
+static int bka_term_putchar(termreg_t *term, char c) {
+    unsigned stat = TERM_TRANSM_STATUS(term);
+
+    if (stat != TERM_ST_READY && stat != TERM_ST_TRANSMITTED)
+        return BKA_E_GEN;
+
+    term->transm_command = TERM_TRANSM_COMMAND(c);
+
+    while ((stat = TERM_TRANSM_STATUS(term)) == TERM_ST_BUSY)
+        ;
+
+    term->transm_command = TERM_CMD_ACK;
+
+    return stat != TERM_ST_TRANSMITTED ? BKA_E_GEN: BKA_E_OK;
+}
+
+static int bka_print_putchar(dtpreg_t *p, char c) {
+	unsigned int status = p->status & PRINT_STATUS_MASK;
+
+	// TODO Can we expand the set of allowable statuses?
+	if (status != PRINT_ST_READY)
+		return BKA_E_GEN;
+
+	p->data0 = c;
+	p->command = PRINT_CMD_PRINTC;
+
+	while ((status = p->status & PRINT_STATUS_MASK) == PRINT_ST_BUSY)
+		;
+
+	p->command = PRINT_CMD_ACK;
+
+	return status != PRINT_ST_READY ? BKA_E_GEN: BKA_E_OK;
 }
