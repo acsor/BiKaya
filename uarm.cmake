9fa21b31a9fef333638fc9e4dc341b38eb921b94
Acsor <nildexo@yandex.com>
2019-12-23 22:36:42 +0100

    Attempting to fix cmake

--- uarm.cmake
+++ uarm.cmake
@@ -2,22 +2,15 @@
 set(BKA_UARM_INC ${PROJECT_SOURCE_DIR}/${BKA_INC}/uarm)
 set(BKA_UARM_SRC ${PROJECT_SOURCE_DIR}/${BKA_SRC}/uarm)
 
-set(CFLAGS_UARM -mcpu=arm7tdmi)
-set(LDFLAGS_UARM -G 0 -nostdlib -T ${BKA_UARM_SRC}/elf32ltsarm.h.uarmcore.x)
+set(CFLAGS_UARM -nostdlib -O0 -mcpu=arm7tdmi)
+set(LDFLAGS_UARM "-nostdlib -Wl,-G,0,-T,${BKA_UARM_SRC}/elf32ltsarm.h.uarmcore.x")
 
-add_compile_options(${CFLAGS_UARM})
+include_directories(${BKA_UARM_INC})
 
-
-add_library(libuarm OBJECT ${BKA_UARM_SRC}/libuarm.s)
-add_library(crtso OBJECT ${BKA_UARM_SRC}/crtso.s)
-
-target_include_directories(libuarm PRIVATE ${BKA_UARM_INC})
-
-add_custom_target(
-	kernel.uarm
-	DEPENDS crtso libuarm io phase0
-	COMMAND ${CMAKE_C_LINKER} ${LDFLAGS_UARM}
-	$<TARGET_OBJECTS:crtso> $<TARGET_OBJECTS:libuarm>
-	$<TARGET_OBJECTS:io> $<TARGET_OBJECTS:phase0>
-	-o kernel.uarm
+add_executable(
+	kernel.uarm ${BKA_SRC}/phase0.c
+	# Libraries the current target depends on
+	${BKA_UARM_SRC}/libuarm.s ${BKA_UARM_SRC}/crtso.s ${BKA_SRC}/io.c
 )
+set_property(TARGET kernel.uarm PROPERTY COMPILE_OPTIONS ${CFLAGS_UARM})
+set_property(TARGET kernel.uarm PROPERTY LINK_FLAGS ${LDFLAGS_UARM})
