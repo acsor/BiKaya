df49df9a9e95fef1132c91918615698eac5a9a9b
Daniele <daniele.morotti.99@gmail.com>
2020-01-01 21:18:45 +0100

    Implementazione bka_atoi10

--- src/string.c
+++ src/string.c
@@ -13,3 +13,17 @@ int bka_strlen (char const *str) {
 
 	return cont;
 }
+
+
+int bka_atoi10(char const *src){
+	int i = 0;
+	int res = 0;
+	
+	while(src[i] != '\0')
+			{
+				res = res*10 + (src[i] - '0');
+				i+=1;
+			}
+			
+	return res;
+}
