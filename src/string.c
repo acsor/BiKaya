353732ac85e545269636709d587dc427d1d82d26
Daniele <daniele.morotti.99@gmail.com>
2020-01-05 17:08:39 +0100

    Implementate le funzioni richieste

--- src/string.c
+++ src/string.c
@@ -1,7 +1,7 @@
 #include "string.h"
+#include <stdbool.h>
 #include "sys.h"
 
-// TODO Implement missing functions
 
 int bka_strlen (char const *str) {
  	int cont = 0;
@@ -45,7 +45,90 @@ int bka_atoi(char const *src, unsigned base){
 }
 
 
+
 long int bka_atoi10 (char const *src) {
 	
 	return bka_atoi(src,10);
+}
+
+
+
+int bka_itoa(int n, char *dest, unsigned length){
+
+	 int i = 0, dim =  0, n_tmp = n; 
+	 bool tronc = false;
+	 
+	 if(n < 0) return BKA_E_GEN;
+	 else if(n == 0){
+	 	dest[i] = '0';
+	 	dest[i+1] = '\0';
+	 	return BKA_E_OK;
+	 	}
+	 else{
+	 	while(n_tmp != 0){			//calcolo la dimensione del n° in input
+	 		n_tmp = n_tmp/10;
+	 		dim ++;
+	 	}
+	 }
+	 
+	 while (n != 0) 
+		{ 
+		    int r = n % 10; 
+		    dest[i++] = r + '0'; 
+		    n = n/10; 
+		    
+		    if(i > length){
+		    	tronc = true;
+		    }
+		} 
+		
+	 int j = 0;
+	 char copy[dim+1]; 		//array per invertire la stringa alla fine
+	 copy[dim] = '\0';
+	 i = dim-1;
+	 
+	 while(i >=0 && j < dim){
+	 	copy[j] = dest[i];
+	 	i--;
+	 	j++;
+	 }
+	 
+	 i = 0;
+	 while(i < dim){
+	 	dest[i] = copy[i];
+	 	i++;
+	 }
+	 
+	 dest[length] = '\0';       //così viene troncato il numero alla lunghezza richiesta
+	 
+	 if(tronc) return BKA_E_GEN;
+	 else return BKA_E_OK;
+}
+
+
+
+int bka_strncpy(char *dest, char const *src, unsigned length){
+	if(length == 0 || *src == '\0') return 0;
+	int count = 0, i =0;		
+	while(src[i] != '\0' && count < length){
+	 	dest[i] = src[i];
+	 	i++;
+	 	count++;
+	 }
+	 dest[i] = '\0';
+	 return count;
+}
+
+
+
+int bka_strcpy(char *dest, char const *src){
+	if(*src == '\0') return 0;
+	int count = 0, i =0;		
+	while(src[i] != '\0'){
+	 	dest[i] = src[i];
+	 	i++;
+	 	count++;
+	 }
+	 dest[i] = '\0';
+	 return count+1;  //ritorna il n° di caratteri copiati incluso il '\0'
 }
