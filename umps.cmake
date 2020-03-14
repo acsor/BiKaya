dbc574dd050102b69ba6f439839dbbc7b1686be3
Acsor <nildexo@yandex.com>
2020-03-14 21:14:15 +0100

    Improve umps2-elf2umps step in umps.cmake
    Specifically
    
    * The definition of the x.core.umps target (where x is a uMPS executable) has been
    automated by a function (less verbose cmake code)
    * x.core.umps is now listed in the all target (e.g. in a Makefile)
    * x.core.umps and x.stab.umps are now recognized as "byproducts" such that
    `make clean` can act upon them

--- umps.cmake
+++ umps.cmake
@@ -1,3 +1,16 @@
+# For each argument passed, apply_elf2umps() interprets it as an uMPS
+# executable and applies the umps2-elf2umps step to it, generating a new target
+function(apply_elf2umps)
+	foreach(target IN LISTS ARGV)
+		add_custom_target(
+			${target}.core.umps ALL
+			COMMAND umps2-elf2umps -k ${PROJECT_BINARY_DIR}/${target}
+			BYPRODUCTS ${target}.core.umps ${target}.stab.umps
+			DEPENDS ${target}
+		)
+	endforeach()
+endfunction()
+
 # Project include, source and library dirs for umips-related files
 set(UMPS_INC ${PROJECT_SOURCE_DIR}/${BKA_INC}/umps)
 set(UMPS_SRC ${PROJECT_SOURCE_DIR}/${BKA_SRC}/umps)
@@ -22,27 +35,14 @@ add_library(crtso ${UMPS_SRC}/crtso.S)
 # Add kernel0.core.umps executable
 add_executable(kernel0 ${BKA_SRC}/phase0.c)
 target_link_libraries(kernel0 crtso libumps io)
-# Run the `umps2-elf2umps -k kernel0' command after building `kernel0'
-add_custom_target(
-	kernel0.core.umps
-	COMMAND umps2-elf2umps -k ${PROJECT_BINARY_DIR}/kernel0
-	DEPENDS kernel0
-)
 
 # Add kernel1.core.umps executable
 add_executable(kernel1 ${BKA_SRC}/phase1.c)
 target_link_libraries(kernel1 crtso libumps io string utils asl)
-add_custom_target(
-	kernel1.core.umps
-	COMMAND umps2-elf2umps -k ${PROJECT_BINARY_DIR}/kernel1
-	DEPENDS kernel1
-)
 
 # Add bka_test.core.umps executable
 add_executable(bka_test ${BKA_SRC}/bka_test.c)
 target_link_libraries(bka_test crtso io libumps string utils)
-add_custom_target(
-	bka_test.core.umps
-	COMMAND umps2-elf2umps -k ${PROJECT_BINARY_DIR}/bka_test
-	DEPENDS bka_test
-)
+
+# To each target listed, apply the umps2-elf2umps final step to it
+apply_elf2umps(kernel0 kernel1 bka_test)
