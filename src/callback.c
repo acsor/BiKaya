6831c6d30c1eeeb5dd5043b8e5b1b7e4e42076c2
busca99 <nicolo.buscaroli@gmail.com>
2020-03-30 17:49:30 +0200

    Implementazione della funzione callback.c

--- src/callback.c
+++ src/callback.c
@@ -2,5 +2,25 @@
 
 
 void bka_na_init(state_t *s, nac_t c) {
-	/* TODO Implement. */
+	
+	#ifdef BKA_ARCH_UMPS
+	s -> pc_epc = c;
+	s -> reg_sp = BKA_RAMTOP;
+	
+	//initialize the status register
+	bka_memset(s -> status, 0, sizeof(unsigned int));
+	
+	#elif defined(BKA_ARCH_UARM)
+	s -> pc = c;
+	s -> sp = BKA_RAMTOP;
+	
+	//initialize the status register
+	bka_memset(s -> cpsr, 0, sizeof(unsigned int));
+	bka_memset(s -> CP15_Control, 0, sizeof(unsigned int));
+	s -> cpsr |= 0x1F;
+	
+	#endif
 }
+
+
+
