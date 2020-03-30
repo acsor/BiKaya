4090bcd96563eafdf7f1d5bbb40535c769409748
Acsor <nildexo@yandex.com>
2020-03-30 11:01:27 +0200

    Replace handler.* with callback.* and add an unimplemented primitive to it

---
+++ include/callback.h
@@ -0,0 +1,33 @@
+#ifndef BKA_CALLBACK_H
+#define BKA_CALLBACK_H
+
+#include "sys.h"
+
+
+/**
+ * Function type for a new area callback (NAC).
+ */
+typedef void (*nac_t)(void);
+
+/**
+ * Initializes the new area (NA) pointed to by @c s in such a way that
+ *
+ * <ul>
+ * 		<li>The new area program counter (PC) points to the code executed by
+ * 		@c c</li>
+ * 		<li>The SP (Stack Pointer) points to @c RAMTOP.<li>
+ * 		<li>The status register is initialized so that
+ * 			<ul>
+ * 				<li>Interrupts are masked/disabled</li>
+ * 				<li>Virtual memory is disabled</li>
+ * 				<li>Kernel mode is enabled</li>
+ * 			</ul>
+ * 		</li>
+ * </ul>
+ * @param s New area to initialize.
+ * @param c Callback code to be executed upon reaching this new area.
+ */
+void bka_na_init(state_t *s, nac_t c);
+
+
+#endif
---
+++ src/callback.c
@@ -0,0 +1,6 @@
+#include "callback.h"
+
+
+void bka_na_init(state_t *s, nac_t c) {
+	/* TODO Implement. */
+}
