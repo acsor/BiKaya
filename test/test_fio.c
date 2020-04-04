f9bc7dd4b91b439549bb5b2cf47eff6cb0179329
Daniele <daniele.morotti.99@gmail.com>
2020-04-04 18:30:04 +0200

    Now you can use '%' character before the format character

--- test/test_fio.c
+++ test/test_fio.c
@@ -6,7 +6,7 @@
 int main () {
 	termreg_t *term0 = (termreg_t*) DEV_REG_ADDR(IL_TERMINAL, 0);
 
-	bka_printf(term0, "ds",12, "ciaone");
+	bka_printf(term0, "%d%s",12, "ciaone");
 	HALT();
 
     return 0;
